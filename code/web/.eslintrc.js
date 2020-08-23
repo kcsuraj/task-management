695e4437a1d256124b2abb0d7f71e821a131903c
Suraj kc <fmi-12@Surajs-MacBook-Air.local>
2020-08-23 10:07:56 +0545

    Setup frontend project in react with linting and code formatting tools

---
+++ code/web/.eslintrc.js
@@ -0,0 +1,26 @@
+/**
+ * ? Reference: https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project
+ */
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'latest',
+    },
+  },
+}
