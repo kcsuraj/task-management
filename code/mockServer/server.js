68e834597d5725eb70a3f327c3c942a4319957ce
Suraj kc <isurajkc@gmail.com>
2020-08-24 13:22:49 +0545

    Fix server json file

--- code/mockServer/server.js
+++ code/mockServer/server.js
@@ -1,18 +1,16 @@
 module.exports = () => {
   return {
     tasks: [
-      [
-        {
-          id: "task-1",
-          task: "Learning react testing library",
-          completed: false,
-        },
-        {
-          id: "task-2",
-          task: "Learning mocha and chai",
-          completed: false,
-        },
-      ],
+      {
+        id: "task-1",
+        task: "Learning react testing library",
+        completed: false,
+      },
+      {
+        id: "task-2",
+        task: "Learning mocha and chai",
+        completed: false,
+      },
     ],
   };
 };
