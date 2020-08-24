b02f8985c147af3ccc2d74d2a0c14de17f784de2
Suraj kc <isurajkc@gmail.com>
2020-08-24 11:53:04 +0545

    Create mock server with json-server

---
+++ code/mockServer/server.js
@@ -0,0 +1,18 @@
+module.exports = () => {
+  return {
+    tasks: [
+      [
+        {
+          id: "task-1",
+          task: "Learning react testing library",
+          completed: false,
+        },
+        {
+          id: "task-2",
+          task: "Learning mocha and chai",
+          completed: false,
+        },
+      ],
+    ],
+  };
+};
